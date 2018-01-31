0088454d210eebdddd4e5966acad79f8fc96412b
richkmeli <richkmeli@gmail.com>
2018-01-31 01:21:35 +0100

    add authentication(db, servlet, html)

--- WebContent/js/richkJS/account.js
+++ WebContent/js/richkJS/account.js
@@ -1,10 +1,26 @@
-function serUserNearBrand() {
+
+function accountInformation() {
 
     $(document).ready(function () {
         $.get("user", /*data,*/ function (data, status) {
 
             user = data[0];
-            document.getElementById("userNearBrand").innerHTML = " - " + user.email + " (ADMIN)";
+            // load name near to the brand
+            var string = " - " + user.email;
+            if (user.isAdmin == true) {
+                string = string +" (ADMIN)";
+            }
+            document.getElementById("userNearBrand").innerHTML = string;
+
+            // manage signup, login, logout
+            document.getElementById("signupNavBar").hidden = true;
+            document.getElementById("loginNavBar").hidden = true;
+            document.getElementById("logoutNavBar").hidden = false;
+
+            // if the user is an ADMIN, then it can see users.html
+            if (user.isAdmin == true) {
+                document.getElementById("usersNavBar").hidden = false;
+            }
 
         }, "json"/*, dataType*/)
             .done(function () {
@@ -12,6 +28,10 @@ function serUserNearBrand() {
             })
             .fail(function () {
                 //alert( "error" );
+                // manage signup, login, logout
+                document.getElementById("signupNavBar").hidden = false;
+                document.getElementById("loginNavBar").hidden = false;
+                document.getElementById("logoutNavBar").hidden = true;
             })
             .always(function () {
                 //alert("finished");
--- WebContent/js/richkJS/devices.js
+++ WebContent/js/richkJS/devices.js
@@ -1,9 +1,50 @@
-import setUserNearBrand from "account.js";
 
-function main() {
-    serUserNearBrand();
+function bodyOnLoad() {
+    accountInformation();
     createDevicesTableHeader();
     loadDevicesTable();
+}
+
+
+function accountInformation() {
+
+    $(document).ready(function () {
+        $.get("user", /*data,*/ function (data, status) {
+
+            user = data[0];
+            // load name near to the brand
+            var string = " - " + user.email;
+            if (user.isAdmin == true) {
+                string = string +" (ADMIN)";
+            }
+            document.getElementById("userNearBrand").innerHTML = string;
+
+            // manage signup, login, logout
+            document.getElementById("signupNavBar").hidden = true;
+            document.getElementById("loginNavBar").hidden = true;
+            document.getElementById("logoutNavBar").hidden = false;
+
+            // if the user is an ADMIN, then it can see users.html
+            if (user.isAdmin == true) {
+                document.getElementById("usersNavBar").hidden = false;
+            }
+
+        }, "json"/*, dataType*/)
+            .done(function () {
+                //alert("second success");
+            })
+            .fail(function () {
+                //alert( "error" );
+                // manage signup, login, logout
+                document.getElementById("signupNavBar").hidden = false;
+                document.getElementById("loginNavBar").hidden = false;
+                document.getElementById("logoutNavBar").hidden = true;
+            })
+            .always(function () {
+                //alert("finished");
+            });
+    });
+
 }
 
 function loadDevicesTable() {
