8b4f38ded9eb6dfa3afdecdd0d954d36b5b83eed
Blade94 <mark.ferrer94@hotmail.it>
2019-08-05 10:27:10 +0200

    - finished allignment for channel server-side.

--- src/main/java/it/richkmeli/rms/web/encryptionKey.java
+++ src/main/java/it/richkmeli/rms/web/encryptionKey.java
@@ -1,11 +1,13 @@
 package it.richkmeli.rms.web;
 
 import it.richkmeli.jframework.crypto.Crypto;
-import it.richkmeli.jframework.util.Logger;
 import it.richkmeli.rms.data.device.DeviceDatabaseManager;
-import it.richkmeli.rms.web.util.ServletException;
+import it.richkmeli.rms.web.response.KOResponse;
+import it.richkmeli.rms.web.response.OKResponse;
+import it.richkmeli.rms.web.response.StatusCode;
 import it.richkmeli.rms.web.util.ServletManager;
 import it.richkmeli.rms.web.util.Session;
+import org.json.JSONObject;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
@@ -32,48 +35,86 @@ public class encryptionKey extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
         HttpSession httpSession = request.getSession();
         Session session = null;
+        PrintWriter out = response.getWriter();
+
         try {
             session = ServletManager.getServerSession(httpSession);
-        } catch (ServletException e) {
-            httpSession.setAttribute("error", e);
-            request.getRequestDispatcher(ServletManager.ERROR_JSP).forward(request, response);
 
-        }
+            Map<String, String> attribMap = ServletManager.doDefaultProcessRequest(request, ServletManager.HTTPVerb.GET);
 
-        try {
-            if (request.getParameterMap().containsKey("id")) {
-
-                String name = request.getParameter("id");
-                name = Crypto.decryptRC4(name, password);
+            if (session.getUser() != null) {
+                String id = attribMap.get("id");
+                id = Crypto.decryptRC4(id, password);
 
                 DeviceDatabaseManager deviceDatabaseManager = session.getDeviceDatabaseManager();
-                String encryptionKey = deviceDatabaseManager.getEncryptionKey(name);
+                String encryptionKey = deviceDatabaseManager.getEncryptionKey(id);
                 if (encryptionKey.isEmpty()) {
                     encryptionKey = "Error";
                 }
-
                 // encrypt key server-side generated or error message with pre-shared password
                 encryptionKey = Crypto.encryptRC4(encryptionKey, password);
-
                 encryptionKey = "$" + encryptionKey + "#";
 
-                PrintWriter out = response.getWriter();
-                out.println(encryptionKey);
-                out.flush();
+                JSONObject encKeyJSON = new JSONObject(encryptionKey);
+
+                String output = ServletManager.doDefaultProcessResponse(request, encKeyJSON.toString());
+                out.println((new OKResponse(StatusCode.SUCCESS, output)).json());
             } else {
-                // argomenti non presenti
-                // TODO rimanda da qualche parte perche c'è errore
-                Logger.error("SERVLET encryptionKey, doGet: argomenti non presenti");
-                httpSession.setAttribute("error", "argomenti non presenti");
-                request.getRequestDispatcher(ServletManager.LOGIN_HTML).forward(request, response);
+                out.println((new KOResponse(StatusCode.NOT_LOGGED)).json());
             }
+
         } catch (Exception e) {
-            Logger.error("SERVLET encryptionKey, doGet", e);
-            httpSession.setAttribute("error", e);
-            request.getRequestDispatcher(ServletManager.ERROR_JSP).forward(request, response);
+            out.println((new KOResponse(StatusCode.GENERIC_ERROR, e.getMessage())).json());
         }
+
+        out.flush();
+        out.close();
+
+//        HttpSession httpSession = request.getSession();
+//        Session session = null;
+//        try {
+//            session = ServletManager.getServerSession(httpSession);
+//        } catch (ServletException e) {
+//            httpSession.setAttribute("error", e);
+//            request.getRequestDispatcher(ServletManager.ERROR_JSP).forward(request, response);
+//
+//        }
+//
+//        try {
+//            if (request.getParameterMap().containsKey("id")) {
+//
+//                String name = request.getParameter("id");
+//                name = Crypto.decryptRC4(name, password);
+//
+//                DeviceDatabaseManager deviceDatabaseManager = session.getDeviceDatabaseManager();
+//                String encryptionKey = deviceDatabaseManager.getEncryptionKey(name);
+//                if (encryptionKey.isEmpty()) {
+//                    encryptionKey = "Error";
+//                }
+//
+//                // encrypt key server-side generated or error message with pre-shared password
+//                encryptionKey = Crypto.encryptRC4(encryptionKey, password);
+//
+//                encryptionKey = "$" + encryptionKey + "#";
+//
+//                PrintWriter out = response.getWriter();
+//                out.println(encryptionKey);
+//                out.flush();
+//            } else {
+//                // argomenti non presenti
+//                // TODO rimanda da qualche parte perche c'è errore
+//                Logger.error("SERVLET encryptionKey, doGet: argomenti non presenti");
+//                httpSession.setAttribute("error", "argomenti non presenti");
+//                request.getRequestDispatcher(ServletManager.LOGIN_HTML).forward(request, response);
+//            }
+//        } catch (Exception e) {
+//            Logger.error("SERVLET encryptionKey, doGet", e);
+//            httpSession.setAttribute("error", e);
+//            request.getRequestDispatcher(ServletManager.ERROR_JSP).forward(request, response);
+//        }
     }
 
     @Override
