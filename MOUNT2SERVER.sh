4837625d690dcbf02257aa6239a7ad18d16b53b6
Riccardo Melioli <richkmeli@gmail.com>
2020-05-05 14:29:02 +0200

    Delete MOUNT2SERVER.sh

--- MOUNT2SERVER.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-scp out/artifacts/Richkware_Manager_Server/Richkware-Manager-Server.war pi@www2.richk.me:/var/lib/tomcat8/webapps/
-#-r
-#/run/user/1000/gvfs/sftp\:host\=www2.richk.me\,user\=pi/var/lib/tomcat8/webapps/
